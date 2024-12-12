befbffa2aa11760877efb9c8b6f3df5453e924d4
ly-schneider <lyschneider@icloud.com>
2024-12-12 22:41:10 +0100

    Batman

---
+++ Flink/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Flink;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "Flink" };
+	}
+}
---
+++ Flink/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Flink;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
